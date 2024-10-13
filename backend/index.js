aa15bbcd74d19207a1f4a68dff8208b65b02917f
Arman Siddiqui <arman@golokait.com>
2024-10-13 18:07:01 +0530

    changes in cors

--- backend/index.js
+++ backend/index.js
@@ -6,43 +6,35 @@ const notesRoutes = require('./routes/notes');
 const cors = require('cors');
 require('dotenv').config();
 
-
 connectToMongo();
-const app = express()
-const port = process.env.PORT;
+const app = express();
+const port = process.env.PORT || 5000; // Fallback to 5000 if PORT is not defined
 
-// app.use(cors({
-//   origin:[],
-//   method : ["POST", "GET","PUT","PATCH","DELETE"],
-//   credentials : true,
-// }))
-app.use(cors());
-// app.use(cors({
-//   origin: 'https://i-notes-frontend.vercel.app',
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   allowedHeaders: ['Content-Type', 'Authorization']
-// }));
 // CORS configuration
 const corsOptions = {
-  origin: ['https://i-notes-frontend.vercel.app', "http://localhost:3000"],
+  origin: "http://localhost:3000",
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
+// Apply CORS middleware with options
 app.use(cors(corsOptions));
 
 // Pre-flight request handling
 app.options('*', cors(corsOptions));
-// Creating routes
+
+// Middleware
 app.use(json());
-app.use('/api/auth',authRoutes);
+
+// Routes
+app.use('/api/auth', authRoutes);
 app.use('/api/notes', notesRoutes);
 
 app.get('/', (req, res) => {
-  res.send('Hello  arman!')
-})
-// module.exports = app;
+  res.send('Hello arman!')
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Server listening on port ${port}`)
+});
